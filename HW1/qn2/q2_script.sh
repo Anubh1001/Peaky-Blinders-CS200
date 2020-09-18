ac739bf0d2677da3b7e4521da9268bd4f7c7622e
Anubh1001 <63910270+Anubh1001@users.noreply.github.com>
2020-09-18 19:40:45 +0530

    qn2 script file added

---
+++ HW1/qn2/q2_script.sh
@@ -0,0 +1,20 @@
+cd
+cd Desktop
+mkdir qn2
+cd qn2
+git init
+touch 11940150.txt 
+echo "iit bhilai">11940150.txt
+cp 11940150.txt 11940370.txt
+cp 11940150.txt 11940380.txt
+mkdir Anubh Ritik Dhruv
+mv 11940150.txt Anubh 
+mv 11940370.txt Ritik
+mv 11940380.txt Dhruv
+git add Anubh
+git commit -m "11940150.txt moved to Anubh"
+git add Ritik
+git commit -m "11940370.txt moved to Ritik"
+git add Dhruv
+git commit -m "11940380.txt moved to Dhruv"
+git graph 
